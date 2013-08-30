92dab4774712c94ca1d350cfe9a3261b943d833a
Thomas Sibley <trs@bestpractical.com>
2013-08-30 14:35:06 -0700

    Implement upgrade history rollups in more CSS and less JS
    For a cleaner implementation all around.  Still kinda ugly, but it's
    fine.

--- share/static/js/util.js
+++ share/static/js/util.js
@@ -30,17 +30,9 @@ function switchVisibility(id1, id2) {
     return false;
 }
 
-/* Show/hide with class */
-
-function toggleClassElement(className) {
-    jQuery("." + className).toggle();
-    if ( jQuery("." + className).is(":hidden") ){
-        jQuery("#parent-" + className).find("a").css("background-image", "url('../../../static/images/css/rolldown-arrow.gif')");
-    }
-    else{
-        jQuery("#parent-" + className).find("a").css("background-image", "url('../../../static/images/css/rollup-arrow.gif')");
-    }
-    return false;
+function toggle_upgrade_history(widget, selector) {
+    jQuery(selector).toggle();
+    jQuery(widget).toggleClass("rolled-up");
 }
 
 /* Classes */
